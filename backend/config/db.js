7672e9d2ef4b72d6640a41db29c323b7fd90d5d0
Umer Farooq <umerfaroooq1212@gmail.com>
2026-01-10 19:41:01 +0500

    Fix MongoDB crash on Azure

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
   }
 };
 
